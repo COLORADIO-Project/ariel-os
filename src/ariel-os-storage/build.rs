6060ab223abe0fc69f626d0701fd0b50fd5e7be6
Jakob Hirschler <jakob.hirschler@posteo.de>
2026-05-04 09:58:11 +0200

    Add working PoC for persistent storage on esp32c6
    (cherry picked from commit bf30440c46ba45a9fe3b62c121e486f3207f0c78)

--- src/ariel-os-storage/build.rs
+++ src/ariel-os-storage/build.rs
@@ -1,12 +1,55 @@
 use std::{env, path::PathBuf};
 
 const KIBIBYTES: u32 = 1024;
+const MIBIBYTES: u32 = 1024 * KIBIBYTES;
 
 fn main() {
     // NOTE(hal): values of `flash_page_size` from the datasheets, confirmed by HAL's constants.
     // Important: only homogeneous flash organizations are currently supported.
     // Trying to restrict the storage size to the subset of homogeneous flash would not work as it
     // could be pushed out of it by a large enough binary.
+
+    let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+    if is_in_current_contexts(&["esp32c6"]) {
+        // PoC assumptions:
+        // - 4 MiB flash
+        // - default factory app partition begins at 0x10000
+        // - reserve the last 64 KiB for storage
+        const FLASH_TOTAL: u32 = 4 * MIBIBYTES;
+        const APP_PARTITION_OFFSET: u32 = 0x0001_0000;
+        const STORAGE_OFFSET: u32 = 0x003F_0000; // start of last 64 KiB
+        const STORAGE_SIZE: u32 = 64 * KIBIBYTES;
+        const FLASH_PAGE_SIZE: u32 = 4 * KIBIBYTES;
+        const MMAP_BASE: u32 = 0x4200_0000;
+
+        assert_eq!(STORAGE_OFFSET % FLASH_PAGE_SIZE, 0);
+        assert_eq!(STORAGE_SIZE % FLASH_PAGE_SIZE, 0);
+        const { assert!(STORAGE_OFFSET >= APP_PARTITION_OFFSET) };
+        const { assert!(STORAGE_OFFSET + STORAGE_SIZE <= FLASH_TOTAL) };
+
+        // Note: we cannot use ".storage > FLASH INSERT AFTER .rodata"
+        // section for ESP32-C6, because that would introduce another mapped
+        // flash segment and trips the bootloader assertion.
+        //
+        // Instead we define absolute linker symbols only.
+        let vaddr_start = MMAP_BASE + (STORAGE_OFFSET - APP_PARTITION_OFFSET);
+        let vaddr_end = vaddr_start + STORAGE_SIZE;
+
+        let storage_x = format!(
+            "\
+PROVIDE(__storage_start = 0x{vaddr_start:08x});
+PROVIDE(__storage_end   = 0x{vaddr_end:08x});
+"
+        );
+
+        std::fs::write(out.join("storage.x"), storage_x).unwrap();
+
+        println!("cargo:rerun-if-env-changed=CARGO_CFG_CONTEXT");
+        println!("cargo:rustc-link-search={}", out.display());
+        return;
+    }
+
     let (storage_size_total, flash_page_size) = if is_in_current_contexts(&[
         "stm32f303cb",
         "stm32f303re",
@@ -32,9 +75,6 @@ fn main() {
 
     // `sequential-storage` needs at least two flash pages.
     assert!(storage_size_total / flash_page_size >= 2);
-
-    // Put the linker script somewhere the linker can find it
-    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
 
     let mut storage_template = std::fs::read_to_string("storage.ld.in").unwrap();
     storage_template = storage_template.replace("${ALIGNMENT}", &format!("{flash_page_size}"));
--- src/ariel-os-storage/src/lib.rs
+++ src/ariel-os-storage/src/lib.rs
@@ -46,6 +46,8 @@ fn flash_range_from_linker() -> Range<u32> {
     const OFFSET: usize = 0x1000_0000;
     #[cfg(context = "stm32")]
     const OFFSET: usize = 0x0800_0000;
+    #[cfg(context = "esp32c6")]
+    const OFFSET: usize = 0x41FF_0000;;
     // Default for platform-independent tooling.
     #[cfg(not(context = "ariel-os"))]
     const OFFSET: usize = 0x0;
